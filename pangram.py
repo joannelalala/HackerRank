81ca551e925ec67ee4f633a70a869462b78d22ee
jenli810006995 <jc3992@rit.edu>
2022-06-05 08:21:29 -0500

    add

---
+++ pangram.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'pangrams' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+#
+
+def pangrams(s):
+    # Write your code here
+    if len(s.replace(' ', '').lower()) == 26:
+        output = "pangram"
+    else:
+        output = "no pangram"
+    return output
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = pangrams(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
